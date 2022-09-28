31973964c46f51f7d987cb6bdc59751f155a0f89
Fernanda Palapa <fernandabqd@gmail.com>
2022-09-27 23:09:46 -0500

    ci:funciones

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 console.log ("Probando 1 2 3");
+//* Funcion de suma
+
+function suma(){
+const inputNum1 = Number(document.getElementById("num1").value);
+const inputNum2 = Number(document.getElementById("num2").value);
+const inputResult = document.getElementById("result");
+    document.getElementById("result").value = inputNum1+inputNum2;
+
+} 
+
+//* Función resta
+
+function resta(){
+    const inputNum1 = Number(document.getElementById("num1").value);
+    const inputNum2 = Number(document.getElementById("num2").value);
+    const inputResult = document.getElementById("result");
+        document.getElementById("result").value = inputNum1 - inputNum2;
+}
+
+//*Función multiplicacion
+
+function multiplicacion(){
+    const inputNum1 = Number(document.getElementById("num1").value);
+    const inputNum2 = Number(document.getElementById("num2").value);
+    const inputResult = document.getElementById("result");
+        document.getElementById("result").value = inputNum1*inputNum2;
+}
+//*Función division
+ function division(){
+    const inputNum1 = Number(document.getElementById("num1").value);
+    const inputNum2 = Number(document.getElementById("num2").value);
+    const inputResult = document.getElementById("result");
+        document.getElementById("result").value = inputNum1/inputNum2;
+
+ }
+
+ //* Division arrow*
+//let suma = (a,b) => a+b;
+//result = function (a,b){
+//    return a+b;
+
+//};
+// result(suma(Number(document.getElementByID("num1").value), Number(document.getElementByID("num2".value));
